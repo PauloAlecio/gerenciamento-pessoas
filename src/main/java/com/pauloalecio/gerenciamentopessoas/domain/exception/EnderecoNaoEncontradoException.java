c8b5493f2b6760a0fbd962fb3aa872d75641e273
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-17 04:37:26 -0300

    cria exceção endereco

---
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/domain/exception/EnderecoNaoEncontradoException.java
@@ -0,0 +1,15 @@
+package com.pauloalecio.gerenciamentopessoas.domain.exception;
+
+public class EnderecoNaoEncontradoException extends EntidadeNaoEncontradaException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EnderecoNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public EnderecoNaoEncontradoException(Long id) {
+		this(String.format("Não existe um cadastro de endereço com id %d", id));
+	}
+	
+}
