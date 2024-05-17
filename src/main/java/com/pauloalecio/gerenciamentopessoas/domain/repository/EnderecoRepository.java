d491a9350a06fd9b5ec6aeea54e385ed0d0cd948
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-17 20:49:17 -0300

    Adiciona buscas personalizadas

--- src/main/java/com/pauloalecio/gerenciamentopessoas/domain/repository/EnderecoRepository.java
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/domain/repository/EnderecoRepository.java
@@ -1,9 +1,13 @@
 package com.pauloalecio.gerenciamentopessoas.domain.repository;
 
 import com.pauloalecio.gerenciamentopessoas.domain.model.Endereco;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+  Optional<List<Endereco>> findByPessoaId(Long pessoaId );
+  Optional<Endereco> findByIdAndPessoaId(Long enderecoId,Long pessoaId );
 }
