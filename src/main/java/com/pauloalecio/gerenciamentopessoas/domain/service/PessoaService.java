70b75cc7e46f3de729f485fdfc48a840eadf936d
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-17 03:14:36 -0300

    cria classe de serviço de pessoa

---
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/domain/service/PessoaService.java
@@ -0,0 +1,46 @@
+package com.pauloalecio.gerenciamentopessoas.domain.service;
+
+import com.pauloalecio.gerenciamentopessoas.domain.exception.PessoaNaoEncontradaException;
+import com.pauloalecio.gerenciamentopessoas.domain.model.Pessoa;
+import com.pauloalecio.gerenciamentopessoas.domain.repository.PessoaRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PessoaService {
+
+  private final PessoaRepository pessoaRepository;
+
+  public List<Pessoa> listarPessoas() {
+    return pessoaRepository.findAll();
+  }
+
+  public Pessoa buscarPessoaPorId(Long id) {
+    return pessoaRepository.findById(id).orElseThrow(() -> new PessoaNaoEncontradaException(id));
+  }
+
+
+  @Transactional
+  public Pessoa criarPessoa(Pessoa pessoa) {
+    return pessoaRepository.save(pessoa);
+  }
+
+
+  @Transactional
+  public Pessoa atualizarPessoa(Pessoa pessoa) {
+    return pessoaRepository.save(pessoa);
+  }
+
+
+  @Transactional
+  public void deletarPessoa(Long id) {
+    boolean exists = pessoaRepository.existsById(id);
+    if (!exists){
+      throw new PessoaNaoEncontradaException(id);
+    }
+    pessoaRepository.deleteById(id);
+  }
+}
