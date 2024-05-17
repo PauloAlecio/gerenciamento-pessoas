9fbcc1bbf9d21f9e1cc289a573f17d7291376f9e
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-17 03:13:50 -0300

    Implementa HATEOAS self links

---
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/ApiLinks.java
@@ -0,0 +1,30 @@
+package com.pauloalecio.gerenciamentopessoas.api.v1;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+import com.pauloalecio.gerenciamentopessoas.api.v1.controller.PessoaController;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiLinks {
+
+  public static final TemplateVariables PAGINACAO_VARIABLES = new TemplateVariables(
+      new TemplateVariable("page", VariableType.REQUEST_PARAM),
+      new TemplateVariable("size", VariableType.REQUEST_PARAM),
+      new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+
+
+  public Link linkToPessoas(String rel) {
+    return linkTo(PessoaController.class).withRel(rel);
+  }
+
+  public Link linkToPessoas() {
+    return linkToPessoas(IanaLinkRelations.SELF.value());
+  }
+
+}
