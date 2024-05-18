207d8306f61a5012a22455020ed922474d203f28
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-17 23:01:15 -0300

    Atualiza arquivo afterMigrate.sql

--- src/main/resources/db/testdata/mysql/afterMigrate.sql
+++ src/main/resources/db/testdata/mysql/afterMigrate.sql
@@ -1,3 +1,5 @@
+set foreign_key_checks = 0;
+
 DELETE FROM pessoa;
 ALTER TABLE pessoa auto_increment = 1;
 
@@ -5,14 +7,16 @@ DELETE FROM endereco;
 ALTER TABLE endereco auto_increment = 1;
 
 	-- Inserir dados de exemplo na tabela Pessoa
-INSERT INTO pessoa (nome, data_nascimento) VALUES
-	('João Silva', '1990-05-15'),
-	('Maria Oliveira', '1985-10-25'),
-	('Carlos Santos', '1978-03-20');
+INSERT INTO pessoa (nome, data_nascimento, endereco_principal_id)
+VALUES
+	('João Silva', '1990-05-15',1),
+	('Maria Oliveira', '1985-10-25',NULL),
+	('Carlos Santos', '1978-03-20',3);
 
 
 	-- Inserir dados de exemplo na tabela Endereco
-INSERT INTO endereco (logradouro, cep, numero, cidade, estado, pessoa_id) VALUES
+INSERT INTO endereco (logradouro, cep, numero, cidade, estado, pessoa_id)
+VALUES
 	('Rua A', '12345-678', '100', 'São Paulo', 'SP', 1),
 	('Rua B', '54321-876', '200', 'Rio de Janeiro', 'RJ', 2),
 	('Avenida C', '98765-432', '300', 'Belo Horizonte', 'MG', 3);
