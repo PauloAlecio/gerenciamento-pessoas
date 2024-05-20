b4ca21620453957bbd7ad34e24a568249cc8ccbb
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-20 02:40:40 -0300

    Atualiza EnderecoController.java

--- src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/controller/EnderecoController.java
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/controller/EnderecoController.java
@@ -10,12 +10,14 @@ import com.pauloalecio.gerenciamentopessoas.domain.service.EnderecoService;
 import com.pauloalecio.gerenciamentopessoas.domain.model.Endereco;
 import jakarta.validation.Valid;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/enderecos")
 public class EnderecoController {
