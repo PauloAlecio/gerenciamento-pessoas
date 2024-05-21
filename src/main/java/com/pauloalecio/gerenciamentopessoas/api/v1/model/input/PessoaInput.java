6b258809f7ba1832c94a7f8876c50c0fa5860727
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-21 01:18:30 -0300

    Adiciona metadados ao PessoaInput

--- src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/input/PessoaInput.java
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/input/PessoaInput.java
@@ -1,5 +1,6 @@
 package com.pauloalecio.gerenciamentopessoas.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -10,13 +11,15 @@ import lombok.Setter;
 @Getter
 public class PessoaInput {
 
-
+  @Schema(example = "João da Silva")
   @NotBlank
   private String nome;
 
+  @Schema(example = "1990-02-03")
   @NotNull
   private LocalDate dataNascimento;
 
+  @Schema(example = "1")
   @NotNull
   private Integer endereco_principal_id;
 
