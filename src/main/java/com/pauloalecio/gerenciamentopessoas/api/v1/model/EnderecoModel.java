53dce1c8e35e305fba4bacfc46869b457d75702d
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-21 01:17:58 -0300

    Adiciona metadados ao model EnderecoModel

--- src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/EnderecoModel.java
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/EnderecoModel.java
@@ -2,9 +2,11 @@ package com.pauloalecio.gerenciamentopessoas.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pauloalecio.gerenciamentopessoas.domain.model.Pessoa;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +18,29 @@ import org.springframework.hateoas.server.core.Relation;
 @Setter
 public class EnderecoModel extends RepresentationModel<EnderecoModel> {
 
+  @Schema(example = "1")
   private Long id;
 
-  private String logradouro;
+  @Schema(example = "38400-000")
+  @NotBlank
   private String cep;
+
+  @Schema(example = "Rua Floriano Peixoto")
+  @NotBlank
+  private String logradouro;
+
+  @Schema(example = "\"1500\"")
+  @NotBlank
   private String numero;
+
+  @Schema(example = "Avaré")
+  @NotBlank
   private String cidade;
+
+  @Schema(example = "SP")
+  @NotBlank
   private String estado;
+
 
 
 
