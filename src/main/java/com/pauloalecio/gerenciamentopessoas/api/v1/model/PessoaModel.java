ec4287f4b7c7cb4bdcabbb2db9cba0ad44a1b3c7
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-21 01:17:33 -0300

    Adiciona metadados ao model PessoaModel

--- src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/PessoaModel.java
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/PessoaModel.java
@@ -1,5 +1,8 @@
 package com.pauloalecio.gerenciamentopessoas.api.v1.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,13 +14,19 @@ import org.springframework.hateoas.server.core.Relation;
 @Setter
 public class PessoaModel extends RepresentationModel<PessoaModel> {
 
+  @Schema(example = "1")
   private Long id;
 
+  @Schema(example = "João da Silva")
+  @NotBlank
   private String nome;
 
+  @Schema(example = "1990-02-03")
+  @NotNull
   private LocalDate dataNascimento;
 
+  @Schema(example = "1")
+  @NotNull
   private Integer endereco_principal_id;
-
 
 }
