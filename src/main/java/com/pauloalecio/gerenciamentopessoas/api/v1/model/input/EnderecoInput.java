017a6453a0c8f9524b492ff3791eb2dc8fa5bec9
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-05-21 01:18:49 -0300

    Adiciona metadados ao EnderecoInput

--- src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/input/EnderecoInput.java
+++ src/main/java/com/pauloalecio/gerenciamentopessoas/api/v1/model/input/EnderecoInput.java
@@ -1,5 +1,6 @@
 package com.pauloalecio.gerenciamentopessoas.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,18 +9,23 @@ import lombok.Setter;
 @Getter
 public class EnderecoInput {
 
+	@Schema(example = "38400-000")
 	@NotBlank
 	private String cep;
 
+	@Schema(example = "Rua Floriano Peixoto")
 	@NotBlank
 	private String logradouro;
 
+	@Schema(example = "\"1500\"")
 	@NotBlank
 	private String numero;
 
+	@Schema(example = "Avaré")
 	@NotBlank
 	private String cidade;
 
+	@Schema(example = "SP")
 	@NotBlank
 	private String estado;
 
